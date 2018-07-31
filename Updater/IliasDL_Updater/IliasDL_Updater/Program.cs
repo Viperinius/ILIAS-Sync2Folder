991dc77b62176be66c7609e488d8c55923740667
Viperinius <41393831+Viperinius@users.noreply.github.com>
2018-07-31 20:00:27 +0200

    Fixed issue to delete the files too soon

--- Updater/IliasDL_Updater/IliasDL_Updater/Program.cs
+++ Updater/IliasDL_Updater/IliasDL_Updater/Program.cs
@@ -31,15 +31,36 @@ namespace IliasDL_Updater
                 var uninstallInfo = UninstallInfo.Find("IliasSync2Folder");
                 if (uninstallInfo != null)
                 {
+                    Console.WriteLine("Uninstalling current ILIAS Sync2Folder version...");
                     Uninstaller uninstaller = new Uninstaller();
                     uninstaller.Uninstall(uninstallInfo);
+                    Console.WriteLine("Uninstalling done.");
                 }
 
                 //launch new version's installer
-                Process.Start(sDestDirectory + sSeparator + "setup.exe");
+                Console.WriteLine("Launching new installer...");
+                var installer = Process.Start(sDestDirectory + sSeparator + "setup.exe");
+                
 
+                installer.WaitForExit(milliseconds: 20000);
+                Process[] processes = Process.GetProcessesByName("dfsvc");
+                if (processes.Length != 0)
+                {
+                    Console.WriteLine("ClickOnce found running");
+
+                    Console.WriteLine("Waiting for the installer to close...");
+                    processes[0].WaitForExit(milliseconds: 20000);
+                    Console.WriteLine("Installer exited.");
+                }
+                else
+                {
+                    Console.WriteLine("ClickOnce not running");
+                }
+
+                
 
                 //cleanup
+                Console.WriteLine("Removing temporary install files...");
                 if (File.Exists(Path.GetTempPath() + sFileName))
                 {
 
@@ -49,6 +70,7 @@ namespace IliasDL_Updater
                 {
                     Directory.Delete(sDestDirectory, recursive: true);
                 }
+                Console.WriteLine("Done.");
             }            
         }
     }
