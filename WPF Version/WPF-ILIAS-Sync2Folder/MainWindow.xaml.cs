e8d5ed6f82dd79b51c62f2fc16afbf3f2d4ae8e0
Viperinius <41393831+Viperinius@users.noreply.github.com>
2018-09-06 22:16:04 +0200

    Added blank WPF version for new UI

---
+++ WPF Version/WPF-ILIAS-Sync2Folder/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using MahApps.Metro;
+
+namespace WPF_ILIAS_Sync2Folder
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSettings_Click(object sender, RoutedEventArgs e)
+        {
+            //get current app style
+            Tuple<AppTheme, Accent> appStyle = ThemeManager.DetectAppStyle(Application.Current);
+            //set specific style
+            ThemeManager.ChangeAppStyle(Application.Current, ThemeManager.GetAccent("Green"), ThemeManager.GetAppTheme("BaseLight"));
+        }
+
+        private void BtnStyle_Click(object sender, RoutedEventArgs e)
+        {
+            StyleChanger styleChanger = new StyleChanger();
+            styleChanger.Show();
+        }
+    }
+}
---
+++ WPF Version/WPF-ILIAS-Sync2Folder/StyleChanger.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using MahApps.Metro;
+
+namespace WPF_ILIAS_Sync2Folder
+{
+    /// <summary>
+    /// Interaktionslogik für StyleChanger.xaml
+    /// </summary>
+    public partial class StyleChanger : MetroWindow
+    {
+        public StyleChanger()
+        {
+            InitializeComponent();
+        }
+
+        private void AccentSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Accent selectedAccent = AccentSelector.SelectedItem as Accent;
+
+            if (selectedAccent != null)
+            {
+                Tuple<AppTheme, Accent> style = ThemeManager.DetectAppStyle(Application.Current);
+                ThemeManager.ChangeAppStyle(Application.Current, selectedAccent, style.Item1);
+            }
+        }
+
+        private void BtnDarkTheme_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void ChangeWindowThemeClick(object sender, RoutedEventArgs e)
+        {
+            Tuple<AppTheme, Accent> style = ThemeManager.DetectAppStyle(Application.Current);
+            ThemeManager.ChangeAppStyle(Application.Current, style.Item2, ThemeManager.GetAppTheme("Base" + ((Button)sender).Content));
+        }
+    }
+}
